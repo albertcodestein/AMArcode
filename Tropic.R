27c17bda49ca97c48bade6a4a2b2a7460700ff3b
albertcodestein <vamsi.kaja@rhsmith.umd.edu>
2019-01-06 17:44:38 -0600

    lag variable regression

--- Tropic.R
+++ Tropic.R
@@ -9,14 +9,25 @@ Tropic$end9 <- ifelse((Tropic$Price * 100) %% 10 == 9, 1, 0)
 
 Tropic$Quarter <- floor((Tropic$Week - 1) %% 52 / 13) + 1
 
-#library(car)
-
 #semi-log regression model
 
-Tropic$Quarter <- relevel(as.factor(Tropic$Quarter), 4)
-Tropic$Store <- relevel(as.factor(Tropic$Store), 15)
+Tropic$Quarter <- relevel(as.factor(Tropic$Quarter), 4) #setting quarter 4 as the reference level
+Tropic$Store <- relevel(as.factor(Tropic$Store), 15) #setting store 15 as the reference level
 
 RegCoeff <- lm(log(Quant) ~ Price + as.factor(Quarter) + as.factor(Deal) + as.factor(Store) 
-               + Week + as.factor(end9), data = Tropic)
+                + as.factor(end9) + Week, data = Tropic)
 
 summary(RegCoeff)
+
+#creating lag sales variable
+
+Tropic2 <- Tropic %>% group_by(Store) %>% mutate(LagQuant = lag(Quant)) %>% 
+           select(-"LagQrt")
+
+RegCoeff2 <- lm(log(Quant) ~ Price + as.factor(Quarter) + as.factor(Deal) + as.factor(Store) 
+               + as.factor(end9) + Week + LagQuant, data = Tropic2)
+
+summary(RegCoeff2)
+
+
+
